f6b43a1798ed73fbebbf8c1fcd42139f9ec8e7aa
jspelecb15 <jspelecb15@scs.local>
2025-07-03 21:35:28 -0700

    3rd na ni

--- routes/api.php
+++ routes/api.php
@@ -8,25 +8,20 @@ use App\Http\Controllers\Api\BookController;
 use App\Http\Controllers\Api\GenreController;
 use App\Http\Controllers\Api\ReviewController;
 
-// Public authentication routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-// Protected routes with Sanctum authentication
 Route::middleware('auth:sanctum')->group(function () {
-    // Auth routes
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/logout-all', [AuthController::class, 'logoutAll']);
     Route::get('/token-status', [AuthController::class, 'tokenStatus']);
 
-    // CRUD Resource routes
     Route::apiResource('authors', AuthorController::class);
     Route::apiResource('books', BookController::class);
     Route::apiResource('genres', GenreController::class);
     Route::apiResource('reviews', ReviewController::class);
 
-    // Helper routes for dropdowns/relationships
     Route::get('/books/helpers/authors', [BookController::class, 'authors']);
     Route::get('/books/helpers/genres', [BookController::class, 'genres']);
     Route::get('/reviews/helpers/books', [ReviewController::class, 'books']);
